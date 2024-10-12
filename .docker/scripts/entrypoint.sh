76c266e52ef0cbd1690c477ef474120b9dbbf6ef
Young Joon Lee <entelecheia@hotmail.com>
2024-10-13 05:49:48 +0900

    Refactor ownership change in entrypoint.sh

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -21,21 +21,21 @@ LOCAL_UID=$(id -u "$USERNAME")
 WORKSPACE_ROOT=${WORKSPACE_ROOT:-""}
 APP_INSTALL_ROOT=${APP_INSTALL_ROOT:-""}
 
-if [ "$USER_UID" != "$LOCAL_UID" ]; then
-    echo "Updating UID and GID to $USER_UID:$USER_UID from $LOCAL_UID:$LOCAL_UID"
-    usermod -u "$USER_UID" "$USERNAME"
-    groupmod -g "$USER_UID" "$USERNAME"
-    echo "Changing ownership of home directory to $USER_UID:$USER_UID"
-    chown -R "$USER_UID:$USER_UID" "/home/$USERNAME"
-    if [ -n "$APP_INSTALL_ROOT" ] && [ -d "$APP_INSTALL_ROOT" ]; then
-        echo "Changing ownership of $APP_INSTALL_ROOT directory to $USER_UID:$USER_UID"
-        chown -R "$USER_UID:$USER_UID" "$APP_INSTALL_ROOT"
-    fi
-    if [ -n "$WORKSPACE_ROOT" ] && [ -d "$WORKSPACE_ROOT" ] && [ "$WORKSPACE_ROOT" != "$APP_INSTALL_ROOT" ]; then
-        echo "Changing ownership of workspace directory [$WORKSPACE_ROOT] to $USER_UID:$USER_UID"
-        chown -R "$USER_UID:$USER_UID" "$WORKSPACE_ROOT"
-    fi
-fi
+# if [ "$USER_UID" != "$LOCAL_UID" ]; then
+#     echo "Updating UID and GID to $USER_UID:$USER_UID from $LOCAL_UID:$LOCAL_UID"
+#     usermod -u "$USER_UID" "$USERNAME"
+#     groupmod -g "$USER_UID" "$USERNAME"
+#     echo "Changing ownership of home directory to $USER_UID:$USER_UID"
+#     chown -R "$USER_UID:$USER_UID" "/home/$USERNAME"
+#     if [ -n "$APP_INSTALL_ROOT" ] && [ -d "$APP_INSTALL_ROOT" ]; then
+#         echo "Changing ownership of $APP_INSTALL_ROOT directory to $USER_UID:$USER_UID"
+#         chown -R "$USER_UID:$USER_UID" "$APP_INSTALL_ROOT"
+#     fi
+#     if [ -n "$WORKSPACE_ROOT" ] && [ -d "$WORKSPACE_ROOT" ] && [ "$WORKSPACE_ROOT" != "$APP_INSTALL_ROOT" ]; then
+#         echo "Changing ownership of workspace directory [$WORKSPACE_ROOT] to $USER_UID:$USER_UID"
+#         chown -R "$USER_UID:$USER_UID" "$WORKSPACE_ROOT"
+#     fi
+# fi
 
 # Run the provided command
 exec gosu "${USERNAME}" "$@"
